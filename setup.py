eba34004a46c20338d693d9fc3fbd3038196de78
Yue Ma <yueyumi@google.com>
2020-06-12 02:21:11 +0000

    first version setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='vm-network-migration',
+   version='1.0',
+   description='',
+   author='',
+   author_email='',
+   packages=['vm_network_migration'],  #same as name
+   install_requires=['google-api-python-client', 'google'], #external packages as dependencies
+)
