3dd52b2d502f601ca4d8a7ca6aed3ff11596f8e6
Yue Ma <yueyumi@google.com>
2020-06-30 17:29:33 +0000

    implement basic methods of interacting with GCP

--- vm_network_migration/unmanaged_instance_group.py
+++ vm_network_migration/unmanaged_instance_group.py
@@ -1,19 +1,137 @@
-from vm_network_migration.instance_group import InstanceGroup
+from vm_network_migration.instance_group import InstanceGroup, \
+    InstanceGroupStatus
+from vm_network_migration.instance import Instance
+from vm_network_migration.errors import *
+from googleapiclient.errors import HttpError
+
 
 class UnmanagedInstanceGroup(InstanceGroup):
     def __init__(self, compute, project, instance_group_name, region, zone):
-        super(UnmanagedInstanceGroup, self).__init__(compute, project, instance_group_name, region, zone)
-        self.instances = []
+        """ Initialize an unmanaged instance group object
+
+        Args:
+            compute: google compute engine
+            project: project name
+            instance_group_name: instance group's name
+            region: region name of the instance group
+            zone: zone name of the instance group
+        """
+        super(UnmanagedInstanceGroup, self).__init__(compute, project,
+                                                     instance_group_name,
+                                                     region, zone)
+        self.instances = self.retrieve_instances()
+        self.original_instance_group_configs = self.get_instance_group_configs()
+        self.current_instance_group_configs = None
+        self.status = self.get_status()
+        self.network = None
 
     def get_instance_group_configs(self):
-        return self.compute.instanceGroups().get(project=self.project, zone=self.zone,
-                                          instanceGroup=self.instance_group_name).execute()
-    def list_instances(self):
-        """List all the instances in this instance group
+        """ Get instance group's configurations
+
+        Returns: instance group's configurations
+
+        """
+        return self.compute.instanceGroups().get(project=self.project,
+                                                 zone=self.zone,
+                                                 instanceGroup=self.instance_group_name).execute()
+
+    def retrieve_instances(self):
+        """Retrieve all the instances in this instance group,
+        and save the instances into a list of Instance objects
+
+        Returns: a list of Instance objects
+
+        """
+        instances = []
+        request = self.compute.instanceGroups().listInstances(
+            project=self.project, zone=self.zone,
+            instanceGroup=self.instance_group_name)
+        while request is not None:
+            response = request.execute()
+
+            for instance_with_named_ports in response['items']:
+                print(instance_with_named_ports)
+                instance_name = \
+                    instance_with_named_ports['instance'].split('instances/')[1]
+                instances.append(
+                    Instance(self.compute, self.project, instance_name,
+                             self.region, self.zone))
+            request = self.compute.instanceGroups().listInstances_next(
+                previous_request=request, previous_response=response)
+        return instances
+
+    def delete_instance_group(self):
+        """ Delete the instance group in the compute engine
+
+        Returns:  a deserialized object of the response
+
+        """
+
+        delete_instance_group_operation = self.compute.instanceGroups().delete(
+            project=self.project,
+            zone=self.zone,
+            instanceGroup=self.instance_group_name).execute()
+        self.operation.wait_for_zone_operation(delete_instance_group_operation)
+        return delete_instance_group_operation
+
+    def create_instance_group(self, configs):
+        """ Create the instance group
+
+        Returns: a deserialized object of the response
+
+        """
+        create_instance_group_operation = self.compute.instanceGroups().insert(
+            project=self.project,
+            zone=self.zone,
+            body=configs)
+        return create_instance_group_operation
+
+    def get_status(self):
+        """ Get the current status of the instance group
+
+        Returns: the instance group's status
+
+        """
+        try:
+            self.get_instance_group_configs()
+        except HttpError as e:
+            error_reason = e._get_reason()
+            print(error_reason)
+            # if instance is not found, it has a NOTEXISTS status
+            if "not found" in error_reason:
+                return InstanceGroupStatus.NOTEXISTS
+            else:
+                raise e
+        return InstanceGroupStatus("EXISTS")
+
+    def add_an_instance(self, instance_selfLink):
+        """ Add an instance into the instance group
+
+        Args:
+            instance_selflink: the instance's selfLink
 
         Returns:
 
         """
+        add_instance_operation = self.compute.instanceGroups().addInstances(
+            project=self.project,
+            zone=self.zone,
+            InstanceGroup=self.instance_group_name,
+            body=instance_selfLink)
+        self.operation.wait_for_zone_operation(add_instance_operation)
+        return add_instance_operation
+
+    def add_all_instances(self):
+        """ Add all the instances in self.instances to the current instance group
+        Returns:
+
+        """
+        for instance in self.instances:
+            try:
+                self.add_an_instance(instance.selfLink)
+            except HttpError:
+                raise AddInstanceToInstanceGroupError(
+                    'Failed to add all instances back to the instance group.')
 
 
 
