8fee416108a36cd45339c079223a784e25b2df3b
Yue Ma <yueyumi@google.com>
2020-07-06 23:18:45 +0000

    add comments

--- vm_network_migration/modules/instance_group.py
+++ vm_network_migration/modules/instance_group.py
@@ -1,11 +1,32 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+""" Instance group class: describe an instance group
+    InstanceGroupStatus class: describe an instance group's current status
+"""
 from enum import Enum
-
 from googleapiclient.http import HttpError
 
 
-
-class InstanceGroup:
+class InstanceGroup(object):
     def __init__(self, compute, project, instance_group_name):
+        """ Initialize an instance group.
+
+        Args:
+            compute: google compute engine
+            project: project ID
+            instance_group_name: name of the instance group
+        """
         self.compute = compute
         self.project = project
         self.instance_group_name = instance_group_name
@@ -34,12 +55,21 @@ class InstanceGroup:
         return InstanceGroupStatus("EXISTS")
 
     def create_instance_group(self, configs):
+        """Abstract method: create an instance group using configs
+
+        Args:
+            configs: instance group's configurations
+        """
         pass
 
     def get_instance_group_configs(self):
+        """ Abstract method: get the instance group's configurations
+        """
         pass
 
     def delete_instance_group(self):
+        """ Abstract method: delete the instance group
+        """
         pass
 
 
