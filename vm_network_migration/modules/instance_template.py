5a1bfc235fb3f82ffeb779e2e9b23f5f9d6660d1
Yue Ma <yueyumi@google.com>
2020-07-06 16:51:33 +0000

    fix edit network link bug

--- vm_network_migration/modules/instance_template.py
+++ vm_network_migration/modules/instance_template.py
@@ -1,25 +1,31 @@
 from vm_network_migration.modules.operations import Operations
+from vm_network_migration.utils import generate_timestamp_string
+
 class InstanceTemplate:
-    def __init__(self, compute, project, instance_template_name, instance_template_body = None):
+    def __init__(self, compute, project, instance_template_name,
+                 instance_template_body=None):
         self.compute = compute
         self.project = project
         self.instance_template_name = instance_template_name
         self.operation = Operations(self.compute, self.project, "", "")
         self.instance_template_body = instance_template_body
+        if self.instance_template_body == None:
+            self.instance_template_body = self.get_instance_template_body()
 
     def get_instance_template_body(self):
-
-        return self.compute.instanceTemplates().get(project=self.project, instanceTemplate=self.instance_template_name).execute()
+        return self.compute.instanceTemplates().get(project=self.project,
+                                                    instanceTemplate=self.instance_template_name).execute()
 
     def insert(self):
-
-
-        insert_operation = self.compute.instanceTemplates().insert(project=self.project,
-                                                     body=self.instance_template_body).execute()
+        insert_operation = self.compute.instanceTemplates().insert(
+            project=self.project,
+            body=self.instance_template_body).execute()
         self.operation.wait_for_global_operation(insert_operation['name'])
         return insert_operation
+
     def delete(self):
-        delete_operation = self.compute.instanceTemplates().delete(project=self.project, instanceTemplate=self.instance_template_name)
+        delete_operation = self.compute.instanceTemplates().delete(
+            project=self.project, instanceTemplate=self.instance_template_name)
 
         self.operation.wait_for_global_operation(delete_operation['name'])
         return delete_operation
@@ -33,8 +39,15 @@ class InstanceTemplate:
                 new_subnetwork_link: the selflink of the subnetwork
 
         """
-        self.instance_template_body['networkInterfaces'][0][
+        self.instance_template_body['properties']['networkInterfaces'][0][
             'network'] = new_network_link
-        self.instance_template_body['networkInterfaces'][0][
+        self.instance_template_body['properties']['networkInterfaces'][0][
             'subnetwork'] = new_subnetwork_link
 
+    def get_selfLink(self):
+        instance_template_body = self.get_instance_template_body()
+        return instance_template_body['selfLink']
+
+    def random_change_name(self):
+        self.instance_template_name = self.instance_template_name + '-' + generate_timestamp_string()
+        self.instance_template_body['name'] = self.instance_template_name
