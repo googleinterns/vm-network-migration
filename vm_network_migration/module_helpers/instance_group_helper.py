953f8265105b1e2597af6b7e9474a7865793afd6
Yue Ma <yueyumi@google.com>
2020-07-14 21:29:25 +0000

    add: initialize an instance group from its selfLink

--- vm_network_migration/module_helpers/instance_group_helper.py
+++ vm_network_migration/module_helpers/instance_group_helper.py
@@ -16,10 +16,10 @@
 from vm_network_migration.modules.regional_managed_instance_group import RegionalManagedInstanceGroup
 from vm_network_migration.modules.unmanaged_instance_group import UnmanagedInstanceGroup
 from vm_network_migration.modules.zonal_managed_instance_group import ZonalManagedInstanceGroup
-
+import re
 
 class InstanceGroupHelper:
-    def __init__(self, compute, project, instance_group_name, region, zone):
+    def __init__(self, compute, project, instance_group_name, region, zone, instance_group_selfLink = None):
         """ Initialize an instance group helper object
 
         Args:
@@ -28,13 +28,24 @@ class InstanceGroupHelper:
             instance_group_name: name of the instance group
             region: region of the instance group
             zone: zone of the instance group
+            instance_group_selfLink: selfLink of the instance group
         """
         self.compute = compute
         self.project = project
-        self.instance_group_name = instance_group_name
-        self.region = region
-        self.zone = zone
-        self.status = None
+        if instance_group_selfLink == None:
+            self.instance_group_name = instance_group_name
+            self.region = region
+            self.zone = zone
+            self.status = None
+        else:
+            zone_match = re.search(r'\/zones\/(.*)\/', instance_group_selfLink)
+            if zone_match != None:
+                self.zone = zone_match[1]
+            region_match = re.search(r'\/regions\/(.*)\/', instance_group_selfLink)
+            if region_match != None:
+                self.region = region_match[1]
+            self.name = instance_group_selfLink.split('/')[-1]
+
 
     def build_instance_group(self) -> object:
         """ Initialize a subclass object of the InstanceGroup.
