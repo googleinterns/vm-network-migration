ad21cf47dbd8e39a8085bc8ed4b55b7b70e3f492
Yue Ma <yueyumi@google.com>
2020-07-21 15:36:17 +0000

    add:get backends feature

---
+++ vm_network_migration/modules/target_pool.py
@@ -0,0 +1,104 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+""" TargetPool class: describes a target pool and its API methods
+
+"""
+from vm_network_migration.handler_helper.selfLink_executor import SelfLinkExecutor
+from vm_network_migration.modules.unmanaged_instance_group import UnmanagedInstanceGroup
+
+
+class TargetPool:
+    def __init__(self, compute, project, target_pool_name, region, network,
+                 subnetwork, preserve_instance_external_ip):
+        """ Initialization
+
+        Args:
+            compute: google compute engine
+            project: project id
+            target_pool_name: name of the target pool
+            region: name of the region
+            network: target network
+            subnetwork: target subnet
+            preserve_instance_external_ip: whether to preserve the external IPs
+            of the instances serving the backends
+        """
+        self.compute = compute
+        self.project = project
+        self.target_pool_name = target_pool_name
+        self.region = region
+        self.network = network
+        self.subnetwork = subnetwork
+        self.target_pool_config = self.get_target_pool_configs()
+        self.operations = None
+        self.preserve_instance_external_ip = preserve_instance_external_ip
+        # The instances which don't belong to any instance groups
+        self.attached_single_instances = []
+        # The instances which belong to one or more unmanaged instance groups
+        self.attached_instances_in_unmanaged_instance_group = []
+        self.attached_managed_instance_groups = []
+        self.attached_unmanaged_instance_groups = []
+
+    def get_target_pool_configs(self):
+        """ Get the configs of the target pool
+
+        Returns: a deserialized python object of the response
+
+        """
+        return self.compute.targetPools().get(
+            project=self.project,
+            region=self.region,
+            targetPool=self.target_pool_name).execute()
+
+    def get_attached_backends(self):
+        """ According to the target pool configs, the attached instances
+        can be found. These instances can be a single instance which does
+        not belong to any instance group, but they can also belongs to an
+        unmanaged instance group or a managed instance group. This function
+        can get all these information and store the Instance objects and
+        Instance group objects into the attributes.
+
+        """
+        instance_group_and_instances = {}
+        for instance_selfLink in self.target_pool_config['instances']:
+            instance_selfLink_executor = SelfLinkExecutor(instance_selfLink,
+                                                          self.network,
+                                                          self.subnetwork,
+                                                          self.preserve_instance_external_ip)
+            instance = instance_selfLink_executor.build_an_instance(
+                self.compute)
+            instance_group_selfLinks = instance.get_referrer_selfLinks()
+            # No instance group is associated with this instance
+            if len(instance_group_selfLinks) == 0:
+                self.attached_single_instances.append(instance)
+            else:
+                for selfLink in instance_group_selfLinks:
+                    if selfLink in instance_group_and_instances:
+                        instance_group_and_instances[selfLink].append(
+                            instance.selfLink)
+                    else:
+                        instance_group_and_instances[selfLink] = [
+                            instance.selfLink]
+
+        for instance_group_selfLink, instance_selfLink_list in instance_group_and_instances.items():
+            instance_group_selfLink_executor = SelfLinkExecutor(
+                instance_group_selfLink, self.network, self.subnetwork,
+                self.preserve_instance_external_ip)
+            instance_group = instance_group_selfLink_executor.build_an_instance_group(
+                self.compute)
+            if isinstance(instance_group, UnmanagedInstanceGroup):
+                self.attached_unmanaged_instance_groups.append(instance_group)
+                self.attached_instances_in_unmanaged_instance_group.extend(
+                    instance_selfLink_list)
+            else:
+                self.attached_managed_instance_groups.append(instance_group)
