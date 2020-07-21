b388ddd452b89ea6c82765cb2196b3aee898afc9
Yue Ma <yueyumi@google.com>
2020-07-21 19:41:19 +0000

    add: codes in the network_migration function

---
+++ vm_network_migration/handlers/target_pool_migration.py
@@ -0,0 +1,108 @@
+import warnings
+
+import google.auth
+from googleapiclient import discovery
+from vm_network_migration.handler_helper.selfLink_executor import SelfLinkExecutor
+from vm_network_migration.modules.target_pool import TargetPool
+
+
+class TargetPoolMigration:
+    def __init__(self, project, target_pool_name, network, subnetwork,
+                 preserve_instance_external_ip, region):
+        """ Initialize a BackendServiceMigration object
+
+        Args:
+            project: project ID
+            backend_service_name: name of the backend service
+            network: target network
+            subnetwork: target subnet
+            preserve_instance_external_ip: whether preserve the external IP
+            of the instances which serves this load balancer
+            region: region of the internal load balancer
+        """
+        self.compute = self.set_compute_engine()
+        self.project = project
+        self.region = region
+        self.network = network
+        self.subnetwork = subnetwork
+        self.target_pool_name = target_pool_name
+        self.preserve_instance_external_ip = preserve_instance_external_ip
+        self.target_pool = TargetPool(self.compute, self.project,
+                                      self.target_pool_name, self.region,
+                                      self.network,
+                                      self.subnetwork,
+                                      self.preserve_instance_external_ip)
+        self.instance_migration_handlers = []
+        self.build_instance_migration_handlers()
+        self.instance_group_migration_handlers = []
+        self.build_instance_group_migration_handlers()
+
+    def set_compute_engine(self):
+        """ Credential setup
+
+        Returns:google compute engine
+
+        """
+        credentials, default_project = google.auth.default()
+        return discovery.build('compute', 'v1', credentials=credentials)
+
+    def build_instance_migration_handlers(self):
+        for selfLink in self.target_pool.attached_single_instances_selfLinks:
+            executor = SelfLinkExecutor(selfLink,
+                                        self.network,
+                                        self.subnetwork,
+                                        self.preserve_instance_external_ip)
+
+            instance_migration_handler = executor.build_instance_migration_handler()
+            self.instance_migration_handlers.append(instance_migration_handler)
+
+    def build_instance_group_migration_handlers(self):
+        for selfLink in self.target_pool.attached_managed_instance_groups_selfLinks:
+            executor = SelfLinkExecutor(selfLink,
+                                        self.network,
+                                        self.subnetwork,
+                                        self.preserve_instance_external_ip)
+
+            instance_group_migration_handler = executor.build_instance_group_migration_handler()
+            self.instance_group_migration_handlers.append(
+                instance_group_migration_handler)
+        for selfLink in self.target_pool.attached_unmanaged_instance_groups_selfLinks:
+            # In the current version, all the instances in the unmanaged instance
+            # groups will be migrated to the new network. But in the next version,
+            # the tool may give the user's options about how to process
+            # these instance groups
+            executor = SelfLinkExecutor(selfLink,
+                                        self.network,
+                                        self.subnetwork,
+                                        self.preserve_instance_external_ip)
+
+            instance_group_migration_handler = executor.build_instance_group_migration_handler()
+            self.instance_group_migration_handlers.append(
+                instance_group_migration_handler)
+
+    def network_migration(self):
+        """ Migrate the backends of the target pool one by one from a legacy
+            network to the target subnet.
+
+        """
+        try:
+            print('Migrating single instance backends')
+            for instance_migration_handler in self.instance_migration_handlers:
+                instance_migration_handler.network_migration()
+                print('Reattaching the instance backend')
+                self.target_pool.add_instance(
+                    instance_migration_handler.get_instance_selfLink())
+
+            print('Migrating instance group backends')
+            for instance_group_migration_handler in self.instance_group_migration_handlers:
+                instance_group_migration_handler.network_migration()
+                print('Reattaching the instance group')
+                instance_group_migration_handler.instance_group.set_target_pool(
+                    self.target_pool.selfLink)
+
+        except Exception as e:
+            warnings.warn(e, Warning)
+            self.rollback()
+
+    def rollback(self):
+        pass
